3612c3bdd4f6bd4870f55697aa13e96792096804
vietle292 <viet.le@zaitenllc.com>
2021-01-15 16:37:07 +0700

    add properties

--- src/main/java/com/spring/springselenium/config/RemoteWebdriverConfig.java
+++ src/main/java/com/spring/springselenium/config/RemoteWebdriverConfig.java
@@ -1,33 +1,42 @@
 package com.spring.springselenium.config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Component;
 
-@Lazy
+import java.net.URL;
+
 @Configuration
-@Profile("!remote")
-public class WebDriverConfig {
+@Lazy
+@Profile("remote")
+public class RemoteWebdriverConfig {
+
+    @Value("${selenium.grid.url}")
+    private URL url;
+
     @Value("${default.timeout:30}")
     private int timeOut;
 
     @Bean
     @ConditionalOnMissingBean
-    public WebDriver chromeDriver(){
-        WebDriverManager.chromedriver().version("87.0.4280.88").setup();
-        return new ChromeDriver();
+    public WebDriver remoteChromeDriver(){
+
+        return new RemoteWebDriver(this.url, DesiredCapabilities.chrome());
     }
     @Bean
     @ConditionalOnProperty(name = "browser" , havingValue = "firefox")
-    public WebDriver firefoxDriver(){
-        WebDriverManager.firefoxdriver().setup();
-        return new FirefoxDriver();
+    public WebDriver remoteFirefoxDriver(){
+        return new RemoteWebDriver(this.url, DesiredCapabilities.firefox());
     }
     @Bean
     public WebDriverWait webDriverWait(WebDriver driver){
